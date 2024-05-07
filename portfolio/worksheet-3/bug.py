d027b94dc313b26cf4bd3b6b84ce3b5cc2b2e617
Ralfs Fadejevs <ralphfdesigns@gmail.com>
2024-05-07 09:48:36 +0200

    added in code with a bug

---
+++ portfolio/worksheet-3/bug.py
@@ -0,0 +1,15 @@
+def multiply(a, b):
+    return a + b
+
+def divide(a, b):
+    return a / b
+
+num1 = 10
+num2 = 5
+
+result_multiply = multiply(num1, num2)
+result_divide = divide(num1, num2)
+
+print(f"Multiplication: {result_multiply}")
+print(f"Division: {result_divide}")
+
