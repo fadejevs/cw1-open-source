25d74fd568be8db285a218f5c7fcdfcfe9177680
Ralfs Fadejevs <ralphfdesigns@gmail.com>
2024-05-07 10:51:04 +0200

    added Doxygen files

---
+++ portfolio/worksheet-4/doxy.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    """
+    This is a function to add two numbers.
+
+    :param a: First number.
+    :param b: Second number.
+    :return: Sum of a and b.
+    """
+    return a + b
